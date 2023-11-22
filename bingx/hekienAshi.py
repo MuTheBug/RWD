e99d02c372ff38ec47c410dd33f8a70eae2711ad
Muha The Bug <bugmuha@gmail.com>
2023-11-22 16:51:24 +0300

    ss

--- bingx/hekienAshi.py
+++ bingx/hekienAshi.py
@@ -8,8 +8,7 @@ def check_signal(symbol):
     timeframes = ['1m','5m','15m','1h','4h','8h']
     for tf in timeframes:
         asset_df = get_kline(symbol=symbol,timeframe=tf)
-        asset_df['RSI'] = ta.rsi(close=asset_df['HA_Close'],length=14)
-        signal = "skip"
+        asset_df['RSI'] = ta.rsi(close=asset_df['close'],length=14)
         
         ha_close = asset_df['HA_Close'].iloc[-1]
         ha_close_1 = asset_df['HA_Close'].iloc[-2]
@@ -18,15 +17,19 @@ def check_signal(symbol):
         ha_close_4 = asset_df['HA_Close'].iloc[-5]
         ha_close_5 = asset_df['HA_Close'].iloc[-6]
         ha_close_6 = asset_df['HA_Close'].iloc[-7]
+        ha_close_7 = asset_df['HA_Close'].iloc[-8]
+        ha_close_7 = asset_df['HA_Close'].iloc[-8]
         
         rsi = asset_df['RSI'].iloc[-1]
         prev_rsi = asset_df['RSI'].iloc[-2]
         
-        condition= ha_close_6<ha_close_5<ha_close_4 < ha_close_3< ha_close_2 < ha_close_1 and (ha_close < ha_close_1)
-        if condition:
+        condition_long=  ha_close_6<ha_close_5<ha_close_4 < ha_close_3< ha_close_2 < ha_close_1 and (ha_close < ha_close_1) #and rsi>70
+        condition_short=  ha_close_6>ha_close_5>ha_close_4 >ha_close_3> ha_close_2 > ha_close_1 and (ha_close > ha_close_1) #and rsi>70
+        if condition_long or condition_short:
             print(f'{symbol} +++++++++++++++++++++ on {tf}')
         else:
-            print(f'skip {symbol}')
+            # print(f'skip {symbol} rsi {rsi}')
+            pass
 # check_signal('BTC-USDT')
 
 
