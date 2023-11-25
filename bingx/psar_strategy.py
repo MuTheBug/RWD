e6b8e99dea1f574f5e8aa573b1f8adcdb8d7f17c
Muha The Bug <bugmuha@gmail.com>
2023-11-25 15:07:41 +0300

    lll

--- bingx/psar_strategy.py
+++ bingx/psar_strategy.py
@@ -8,10 +8,17 @@ def deep_dip_strategy(sy):
     timeframes = ['1h']
     for tf in timeframes:
         df = get_kline(sy,tf)
-        macd = macd_stoch_strategy(df)
+        macd_short = macd_stoch_strategy_short(df)
+        macd_long = macd_stoch_strategy_long(df)
         psar = psar_stoch_strategy(df)
-        if macd:
+        if macd_short:
             print(f"{sy} got a MACD Bearish on timeframe : {tf}")
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+        if macd_long:
+            print(f"{sy} got a MACD Bullish on timeframe : {tf}")
             print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
             print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
             print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
@@ -36,7 +43,7 @@ def psar_stoch_strategy(df):
         return (stoch['STOCHd_14_3_3'].iloc[-1] < 70 and stoch['psr'].iloc[-1] > stoch['close'].iloc[-1]) and (stoch['psr'].iloc[-2] < stoch['close'].iloc[-2])
     
         
-def macd_stoch_strategy(df):
+def macd_stoch_strategy_short(df):
         stoch = ta.stoch(high=df['high'],low=df['low'],close=df['close'])
         macd = ta.macd(close=df['close'])
         stoch['MACD'] = macd['MACDh_12_26_9']
@@ -44,6 +51,16 @@ def macd_stoch_strategy(df):
         macd_beaish = stoch['MACD'].iloc[-1] < 0 and stoch['MACD'].iloc[-2] > 0
 
         return stoch['STOCHd_14_3_3'].iloc[-1] >70 and macd_beaish
+    
+        
+def macd_stoch_strategy_long(df):
+        stoch = ta.stoch(high=df['high'],low=df['low'],close=df['close'])
+        macd = ta.macd(close=df['close'])
+        stoch['MACD'] = macd['MACDh_12_26_9']
+        stoch['close']= df['close']
+        macd_beaish = stoch['MACD'].iloc[-1] > 0 and stoch['MACD'].iloc[-2] < 0
+
+        return stoch['STOCHd_14_3_3'].iloc[-1] >30 and macd_beaish
     
 
    
