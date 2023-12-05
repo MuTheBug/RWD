e0717f6295518d8e27d380a6e30573fba6fea96c
Muha The Bug <bugmuha@gmail.com>
2023-12-05 16:49:02 +0300

    dsa;dwqsa

--- RWD-main/bingx/indicators.py
+++ RWD-main/bingx/indicators.py
@@ -5,7 +5,7 @@ def macd_signal(df):
     macd = ta.macd(close=df['close'])
     # macd['signal'] = (macd['MACDh_12_26_9'] < 0) & (macd['MACDh_12_26_9'].shift(1) > 0)
     # macd['signal'] = (macd['MACDh_12_26_9'] < macd['MACDh_12_26_9'].shift(1))
-    return macd['MACDh_12_26_9'].iloc[-1] < macd['MACDh_12_26_9'].iloc[-2]< macd['MACDh_12_26_9'].iloc[-3]
+    return macd['MACDh_12_26_9'].iloc[-1] > macd['MACDh_12_26_9'].iloc[-2]> macd['MACDh_12_26_9'].iloc[-3]
 
 def stoc_signal_above_80(df):
     st= ta.stoch(high=df['high'],low=df['low'],close=df['close'])
