83575e0fc764078219acb69863e2f8eec112323c
Muha The Bug <bugmuha@gmail.com>
2023-12-21 12:59:17 +0300

    kiroekmfle

--- RWD-main/bingx/indicators.py
+++ RWD-main/bingx/indicators.py
@@ -4,12 +4,31 @@ from imports import *
 import get_klines
 def just_crossed_up(df):
     macd = ta.macd(close=df['close'])
-    return macd['MACDh_12_26_9'].iloc[-1] > 0 and macd['MACDh_12_26_9'].iloc[-2]< 0
-    
+    condition_1 =  macd['MACDh_12_26_9'].iloc[-1] > 0 and macd['MACDh_12_26_9'].iloc[-2]< 0
+    condition_2 =  macd['MACDh_12_26_9'].iloc[-2] > 0 and macd['MACDh_12_26_9'].iloc[-3]< 0
+    return condition_1 or condition_2
+
+def is_macd__going_down_on_daily(df):
+    macd = ta.macd(close=df['close'])
+    condition_1 =  macd['MACDh_12_26_9'].iloc[-1] < macd['MACDh_12_26_9'].iloc[-2] and macd['MACDh_12_26_9'].iloc[-2] > macd['MACDh_12_26_9'].iloc[-3] and ['MACDh_12_26_9'].iloc[-1] > 0 
+    condition_2 =  macd['MACDh_12_26_9'].iloc[-1] < macd['MACDh_12_26_9'].iloc[-2] and macd['MACDh_12_26_9'].iloc[-2] < macd['MACDh_12_26_9'].iloc[-3] and  macd['MACDh_12_26_9'].iloc[-3] > macd['MACDh_12_26_9'].iloc[-4] and macd['MACDh_12_26_9'].iloc[-3] >0
+    if condition_1 or condition_2:
+        return True
+    return False
+
+def is_macd__going_up_on_daily(df):
+    macd = ta.macd(close=df['close'])
+    condition_1 =  macd['MACDh_12_26_9'].iloc[-1] > macd['MACDh_12_26_9'].iloc[-2] and macd['MACDh_12_26_9'].iloc[-2] < macd['MACDh_12_26_9'].iloc[-3] and ['MACDh_12_26_9'].iloc[-1] < 0 
+    condition_2 =  macd['MACDh_12_26_9'].iloc[-1] > macd['MACDh_12_26_9'].iloc[-2] and macd['MACDh_12_26_9'].iloc[-2] > macd['MACDh_12_26_9'].iloc[-3] and  macd['MACDh_12_26_9'].iloc[-3] < macd['MACDh_12_26_9'].iloc[-4] and macd['MACDh_12_26_9'].iloc[-3] <0
+    if condition_1 or condition_2:
+        return True
+    return False
 
 def just_crossed_down(df):
     macd = ta.macd(close=df['close'])
-    return macd['MACDh_12_26_9'].iloc[-1] < 0 and macd['MACDh_12_26_9'].iloc[-2]> 0
+    condition_1 =  macd['MACDh_12_26_9'].iloc[-1] < 0 and macd['MACDh_12_26_9'].iloc[-2]> 0
+    condition_2 =  macd['MACDh_12_26_9'].iloc[-2] < 0 and macd['MACDh_12_26_9'].iloc[-3]> 0
+    return condition_1 or condition_2
 
 
 def already_up_going_down(df):
--- RWD-main/bingx/main.py
+++ RWD-main/bingx/main.py
@@ -3,8 +3,8 @@ from imports import *
 from indicators import *
         
 def long_on_one_day(symbol,four_hours,daily):
-    x = just_crossed_up(daily)
-    x1= already_down_going_up(four_hours)
+    x = is_macd__going_up_on_daily(daily)
+    x1 = just_crossed_up(four_hours)
     condition = x and x1
     if condition:
         print(f"go long on {symbol} on one day")
@@ -15,8 +15,8 @@ def long_on_one_day(symbol,four_hours,daily):
     
  
 def short_on_one_day(symbol,four_hours,daily):
-    y = just_crossed_down(daily)
-    y1 = already_up_going_down(four_hours)
+    y = is_macd__going_down_on_daily(daily)
+    y1 = just_crossed_down(four_hours)
     condition = y and y1
     if condition:
         print(f"go short on {symbol} on one day")
@@ -25,15 +25,7 @@ def short_on_one_day(symbol,four_hours,daily):
     else:
         return False
   
-def ema_with_mac(symbol, daily, four_hours):
-    ema_cross = ema_cross_up(daily)
-    four = already_down_going_up(four_hours)
-    if  ema_cross:
-        print(f"go long on {symbol} on one day EMA CROSS")
-        send_to_telegram(f"go long on {symbol} on one day EMA CROSS")
-        return True
-    else:
-        return False
+
         
 def main(symbol):
     daily = get_kline(symbol,'1d')
@@ -41,9 +33,8 @@ def main(symbol):
 
     one = long_on_one_day(symbol,four_hours,daily)
     three = short_on_one_day(symbol,four_hours,daily)
-    four = ema_with_mac(symbol,daily,four_hours)
 
-    if not one and  not three and not four:
+    if not one and  not three:
         print(f'skip {symbol}')
     # sleep(1)
 
